969624b00a59dbf1eac1833a8ecb2c568fcc2cbd
github username <github email address>
2018-08-13 10:23:03 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,31 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(let i=1; i < arr.length; i++){
+    let j = i;
+    while(j> 0 && arr[j-1] > arr[j]){
+      [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
+      j--
+    }
+
+  }
 }
+
+
+// function bubblesort(arr) {
+//   // your code here!
+//   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+//   let change;
+//   let x = arr.length
+//   do{
+//     change = 0;
+//     for(let i = 1; i< x; i++){
+//       if(arr[i-1] > arr[i]){
+//         [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
+//         change++;  
+//       } 
+//     }
+//     x--
+//     }while (change !== 0 )
+//     return arr
+// }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,17 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+
+  for(let i = 0; i < arr.length - 1; i++){
+    let smallestIndex = i;
+    for(let j = i+1; j< arr.length; j++){
+      if(arr[j] < arr[smallestIndex]){
+        smallestIndex = j
+      }
+    }
+    [arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]]
+  }
+  return arr;
 }
+
