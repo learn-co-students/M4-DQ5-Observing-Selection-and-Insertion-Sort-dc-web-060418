13d96c133bb9c876ef6197cf9a327d97a945b363
github username <github email address>
2018-08-13 10:19:02 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,17 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+  for(let i = 1; i < arr.length; i++) {
+    for(let j = (i); j>0; j--) {
+      if (arr[j] < arr[j-1]){
+        [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+      }
+    }
+  }
+  return arr
 }
+
+// let x = arr[j]
+// arr[j] = arr[j-1]
+// arr[j-1] = x
